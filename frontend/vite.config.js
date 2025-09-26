bf97104f589abf4a1b55ea54351a1c0c7dfa1914
ezekill34 <jhonsantiagograndezmaguina@gmail.com>
2025-09-26 10:45:04 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ IMPORTANTE: cambia "web" si tu repo en GitHub se llama diferente
+
 export default defineConfig({
   base: "/web/",
   plugins: [react()],
